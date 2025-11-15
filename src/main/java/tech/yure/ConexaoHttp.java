71bedc23f6b5159b001c1fe8b598d443e34e490e
Yure <yure.nathan303@gmail.com>
2025-11-15 04:00:32 -0300

    feat: Initial commit

---
+++ src/main/java/tech/yure/ConexaoHttp.java
@@ -0,0 +1,31 @@
+package tech.yure;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConexaoHttp {
+    private HttpClient client;
+
+    public ConexaoHttp() {
+        this.client = HttpClient.newHttpClient();
+    }
+
+    public String responseApi(String baseUrl){
+        HttpResponse<String> response = null;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(baseUrl))
+                .GET()
+                .build();
+
+        try{
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            System.out.println("Erro ao consultar dados... Verifique a conexão da API!");
+        }
+        return response.body();
+    }
+
+}
---
+++ src/main/java/tech/yure/Main.java
@@ -0,0 +1,7 @@
+package tech.yure;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
